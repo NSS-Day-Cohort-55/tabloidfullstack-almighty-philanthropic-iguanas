fd106761e140c58205a26ba7198fa73f61903e09
JavontaeH <hardeman.javontae@gmail.com>
2022-07-12 15:56:25 -0500

    finished adding read time to post

--- Tabloid/client/src/components/Helpers.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const RemoveTimeFromDateTime = (date) => {
-    let dateString = date.toString();
-    let splitArray = dateString.split("T");
-    return splitArray[0];
-}
