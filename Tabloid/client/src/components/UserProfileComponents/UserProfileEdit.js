1a414bab35c2126b6e436102d276e89cc7475a7e
rushayhays <rushayhays@outlook.com>
2022-07-14 09:56:41 -0500

    form now tracks all changes and throws no errors

--- Tabloid/client/src/components/UserProfileComponents/UserProfileEdit.js
+++ Tabloid/client/src/components/UserProfileComponents/UserProfileEdit.js
@@ -16,12 +16,14 @@ export default function UserProfileEdit(){
             userType:{
                 name:""
             },
+            userTypeId:0,
             createDateTime:""
         });
     
     useEffect(()=>{
         getUserProfileById(id).then(profile => {
             setProfile(profile);
+            
         })
     }, []);
 
@@ -30,9 +32,25 @@ export default function UserProfileEdit(){
         profileToEdit[profileForm.target.id] = profileForm.target.value;
         setProfile(profileToEdit)
     }
+    const handleSelect = (profileForm) => {
+        const profileToEdit = {...profile};
+        let userTypeElement = document.getElementById("userType")
+        let value = userTypeElement.value
+        
+        if(value == 1){
+            profileToEdit.userType.name = "Admin"
+            profileToEdit.userType.id = 1
+            profileToEdit.userTypeId = 1
+            setProfile(profileToEdit)
+        }else{
+            profileToEdit.userType.name = "Author"
+            profileToEdit.userType.id = 2
+            profileToEdit.userTypeId = 2
+            setProfile(profileToEdit)
+        }
+    }
 
     const saveChanges = () =>{
-        console.log("goody");
         console.log(profile);
     }
 
@@ -44,55 +62,55 @@ export default function UserProfileEdit(){
 
             <div className="userProfileEditFormBody">
                 {/* The Id is stored here, but not allowed to be changed */}
-                <input onChange={handleEdit} htmlFor="Id" type="hidden" class="form-control" />
-                <div class="form-group">
+                <input onChange={handleEdit} htmlFor="Id" type="hidden" className="form-control" />
+                <div className="form-group">
                     <h5>FirstName</h5>
-                    <label htmlFor="firstName" class="control-label"></label>
-                    <input onChange={handleEdit} type="text" id="firstName" name="firstName" class="form-control" value={profile.firstName} />
-                    {/* <span asp-validation-for="FirstName" class="text-danger"></span> */}
+                    <label htmlFor="firstName" className="control-label"></label>
+                    <input onChange={handleEdit} type="text" id="firstName" name="firstName" className="form-control" value={profile.firstName} />
+                    {/* <span asp-validation-for="FirstName" className="text-danger"></span> */}
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h5>LastName</h5>
-                    <label htmlFor="lastName" class="control-label"></label>
-                    <input onChange={handleEdit} type="text" id="lastName" name="lastName" class="form-control" value={profile.lastName} />
-                    {/* <span asp-validation-for="LastName" class="text-danger"></span> */}
+                    <label htmlFor="lastName" className="control-label"></label>
+                    <input onChange={handleEdit} type="text" id="lastName" name="lastName" className="form-control" value={profile.lastName} />
+                    {/* <span asp-validation-for="LastName" className="text-danger"></span> */}
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h5>Display Name</h5>
-                    <label htmlFor="displayName" class="control-label"></label>
-                    <input onChange={handleEdit} type="text" id="displayName" name="displayName" class="form-control" value={profile.displayName} />
-                    {/* <span asp-validation-for="DisplayName" class="text-danger"></span> */}
+                    <label htmlFor="displayName" className="control-label"></label>
+                    <input onChange={handleEdit} type="text" id="displayName" name="displayName" className="form-control" value={profile.displayName} />
+                    {/* <span asp-validation-for="DisplayName" className="text-danger"></span> */}
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h5>Email</h5>
-                    <label htmlFor="email" class="control-label"></label>
-                    <input onChange={handleEdit} type="text" id="email" name="email" class="form-control" value={profile.email}/>
-                    {/* <span asp-validation-for="Email" class="text-danger"></span> */}
+                    <label htmlFor="email" className="control-label"></label>
+                    <input onChange={handleEdit} type="text" id="email" name="email" className="form-control" value={profile.email}/>
+                    {/* <span asp-validation-for="Email" className="text-danger"></span> */}
                 </div>
-                <input onChange={handleEdit} htmlFor="CreateDateTime" type="hidden" class="form-control" />
-                <div class="form-group">
+                <input onChange={handleEdit} htmlFor="CreateDateTime" type="hidden" className="form-control" />
+                <div className="form-group">
                     <h5>Image Location</h5>
-                    <label htmlFor="imageLocation" class="control-label"></label>
-                    <input onChange={handleEdit} type="text" id="imageLocation" name="imageLocation" class="form-control" value={profile.imageLocation}/>
-                    {/* <span asp-validation-for="ImageLocation" class="text-danger"></span> */}
+                    <label htmlFor="imageLocation" className="control-label"></label>
+                    <input onChange={handleEdit} type="text" id="imageLocation" name="imageLocation" className="form-control" value={profile.imageLocation}/>
+                    {/* <span asp-validation-for="ImageLocation" className="text-danger"></span> */}
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h5>User Type</h5>
-                    <label htmlFor="UserTypeId" class="control-label"></label>
-                    <select id="userType" htmlFor="UserTypeId" class="form-control" onchange="changeUserType()">
+                    <label htmlFor="UserTypeId" className="control-label"></label>
+                    <select id="userType" htmlFor="UserTypeId" className="form-control" onChange={handleSelect}>
                         <option id="admin" value="1">Administrator</option>
                         <option id="auth" value="2">Author</option>
                     </select>
-                    {/* <span asp-validation-for="UserTypeId" class="text-danger"></span> */}
+                    {/* <span asp-validation-for="UserTypeId" className="text-danger"></span> */}
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <h5>Status (The User is Active)</h5>
-                    <label htmlFor="isActive" class="control-label"></label>
-                    <input onChange={handleEdit} type="text" id="isActive" name="isActive" class="form-control" value={profile.isActive} />
-                    {/* <span asp-validation-for="DemoteVoteOne" class="text-danger"></span> */}
+                    <label htmlFor="isActive" className="control-label"></label>
+                    <input onChange={handleEdit} type="text" id="isActive" name="isActive" className="form-control" value={profile.isActive} />
+                    {/* <span asp-validation-for="DemoteVoteOne" className="text-danger"></span> */}
                 </div>
                 
-                <div class="form-group">
+                <div className="form-group">
                     <button onClick={saveChanges}>Save Changes</button>
                 </div>
             </div>
